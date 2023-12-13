597b944e0e6b220e56a8ca6ba0859b8a3e64be50
ZsomborTLP <faur010zsombor@gmail.com>
2023-12-13 13:16:39 +0100

    Delete gombb a kosárba

--- Kosarba.js
+++ Kosarba.js
@@ -1,6 +1,6 @@
 
 import React, { useEffect, useState } from 'react';
-import { View, Text, StyleSheet, Image, DefaultTheme, FlatList,TouchableOpacity } from 'react-native';
+import { View, Text, StyleSheet, Image, DefaultTheme, FlatList, TouchableOpacity, Button } from 'react-native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import Ipcim from './Ipcim';
 import { LinearGradient } from 'expo-linear-gradient';
@@ -21,7 +21,7 @@ const Kosarba = ({ route }) => {
     function megvetted({ }) {
         Alert("Megvetted")
 
-    } 
+    }
     return (
         <LinearGradient
             colors={['#AAD8E6', '#90EE90']}
@@ -36,6 +36,9 @@ const Kosarba = ({ route }) => {
                             <View>
                                 <Text style={{ fontSize: 20, textAlign: 'center' }}>{item[0]}</Text>
                                 <Text style={{ fontSize: 20, textAlign: 'center' }}>{item[1]} FT</Text>
+                                <TouchableOpacity style={{ marginLeft: 300, backgroundColor: "red", width: 45, height: 30, paddingTop:8.5,borderRadius:20}} onPress={() => torolted()}>
+                                    <Text style={{ color: "white", textAlign: "center", fontSize: 8 }} >Törlés</Text>
+                                </TouchableOpacity>
 
                                 <View style={{ marginTop: 15, marginBottom: 10, borderColor: 'gray', borderWidth: 1 }}></View>
                             </View>
@@ -45,11 +48,11 @@ const Kosarba = ({ route }) => {
 
                 </View>
                 <View>
-                
-                <TouchableOpacity style={{marginRight:'auto',marginLeft:'auto', backgroundColor: "green", width: 105, height: 45, padding: 10 }} onPress={() => megvetted()}>
-                    <Text style={{ color: "white", textAlign: "center", fontSize: 15 }} >Vásárlás</Text>
-                </TouchableOpacity>
-                <Image source={{ uri: `${Ipcim.Ipcim}logo.jpg` }} style={{ width: 75, height: 75, borderRadius: 55, marginBottom: 25, marginLeft: 25,marginTop:-50 }} />
+
+                    <TouchableOpacity style={{ marginRight: 'auto', marginLeft: 'auto', backgroundColor: "green", width: 105, height: 45, padding: 10 }} onPress={() => megvetted()}>
+                        <Text style={{ color: "white", textAlign: "center", fontSize: 15 }} >Vásárlás</Text>
+                    </TouchableOpacity>
+                    <Image source={{ uri: `${Ipcim.Ipcim}logo.jpg` }} style={{ width: 75, height: 75, borderRadius: 55, marginBottom: 25, marginLeft: 25, marginTop: -50 }} />
                 </View>
             </ScrollView>
 
